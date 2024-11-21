800ba97446357d0373b5e76de74d625b3353c404
Hordunlarmy <Hordunlarmy@gmail.com>
2024-11-21 07:16:07 +0100

    refactor(LRUCache): Improve LRU cache implementation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -22,17 +22,14 @@ class LRUCache(BaseCaching):
         """
         if key is None or item is None:
             return
-
-        if (
-            key not in self.cache_data
-            and len(self.cache_data) >= BaseCaching.MAX_ITEMS
-        ):
-            # Remove the least recently used item
-            lru_key, _ = self.cache_data.popitem(last=False)
-            print("DISCARD:", lru_key)
-
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=False)
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Retrieves an item by key.
@@ -43,8 +40,7 @@ class LRUCache(BaseCaching):
         Returns:
             The item associated with the key or None if not found.
         """
+
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
-            return self.cache_data[key]
-
-        return None
+        return self.cache_data.get(key, None)
