d86f1c1815d556185a3e86a86af6259210563cec
Hordunlarmy <Hordunlarmy@gmail.com>
2024-11-21 07:08:54 +0100

    feat: Implement LIFOCache class for LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Last-In First-Out caching module."""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO cache implementation with a limit on stored items."""
+
+    def __init__(self):
+        """Initializes the cache."""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item to the cache with LIFO eviction policy.
+
+        Args:
+            key: The key to store the item under.
+            item: The item to be stored.
+        """
+        if key is None or item is None:
+            return
+
+        if (
+            key not in self.cache_data
+            and len(self.cache_data) >= BaseCaching.MAX_ITEMS
+        ):
+            # Remove the last item in the cache
+            last_key, _ = self.cache_data.popitem(last=True)
+            print("DISCARD:", last_key)
+
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Retrieves an item by key.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item associated with the key or None if not found.
+        """
+        return self.cache_data.get(key)
